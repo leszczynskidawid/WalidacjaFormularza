28e3bde3343e3c006500506664a4768282ac13d7
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2022-01-25 08:59:32 +0100

    master commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {  
+    nameUser:"",
+    emailUser:"",
+    passwordUser:""
+  }  
+  handleonChange  = (e) => {
 
+const input_name = e.target.name;  
+const value = e.target.value; 
+this.setState({
+[input_name]:value,
+
+  
+
+})
+  };
+  render() { 
+    return (
+    <div>
+      <form>
+      <input type="text" placeholder='name' value={this.state.nameUser}  name ='nameUser' onChange = {this.handleonChange}/>
+      <input type="email" placeholder='email'value={this.state.emailUser}  name ='emailUser'onChange = {this.handleonChange}/>
+      <input type="password" placeholder='password' value={this.state.passwordUser} name ='passwordUser' onChange = {this.handleonChange}/>
+      <button className='login'>LOg In</button>
+      <div>
+        <input type="checkbox" name = "age" />
+        <label htmlFor="scales">Mam ukończone 18 lat</label>
+      </div>
+      </form>
+    </div>
+    
+   
+    );
+  }
+}
+ 
 export default App;
